0e1bf560a18137136b09da07e05e35b8f6f8528d
andi.aditya <andi.aditya@saga.id>
2023-01-07 09:51:27 +0700

    chore: add dynamic db name config

--- cmd/models/setup.go
+++ cmd/models/setup.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open(mysql.Open(os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASS") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/golang?charset=utf8&parseTime=True&loc=Local"))
+	database, err := gorm.Open(mysql.Open(os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASS") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8&parseTime=True&loc=Local"))
 
 	if err != nil {
 		panic("Failed connect to database")
